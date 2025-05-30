e2fce37e0c1e4e35193b36d37a25cb6bafb2f1d0
AzureAD\ManenoMgwami <Maneno.Mgwami@osiristrading.com>
2025-05-30 14:35:50 +0200

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  
+server: {
+  host: '0.0.0.0',
+  port: 8080,
+  allowedHosts: true
+}
+
 });
