e22684c2d7f443f4072f3c05b77c6a24462508d0
AzureAD\ManenoMgwami <Maneno.Mgwami@osiristrading.com>
2025-05-31 15:03:06 +0200

    fixing spelling error

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("login", "components/Login.tsx"),
+    route("login", "components/LogIn.tsx"),
     route("registerlandlord", "components/RegisterLandLord.tsx"),
     route("landlord/:landlordId", "components/LandLordPage.tsx")
 ] satisfies RouteConfig;
