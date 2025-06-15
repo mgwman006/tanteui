1c498e04733743af76816a8f039a3e1e0a19f1d7
AzureAD\ManenoMgwami <Maneno.Mgwami@osiristrading.com>
2025-06-15 14:22:38 +0200

    updated page

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -6,7 +6,7 @@ import { isMobile, isTablet, isBrowser } from 'react-device-detect';
 const projectsList = [
     {
         title: 'Lets Explore Tanzania',
-        description:'This is a partnership agreement with Lets Explore Tanzania, its a tour operator entity in tanzania. Our responsibility is to apply modern technologies for improving tourism experience in Tanzania',
+        description:'This is a partnership agreement with Lets Explore Tanzania, its a tour operator entity in tanzania. Our responsibility is to apply modern technologies for improving tourism experience',
         imagePath:'letsexp.jpeg',
         link: "http://www.letsexploretanzania.co.tz"
     },
@@ -17,46 +17,62 @@ const projectsList = [
         link:""
     }
 ];
+
 export default function Projects()
 {
     return (
-        <div style={{width:'100%', padding:'20px'}}>
+        <div style={
+            {
+                width:'100%', 
+                paddingLeft:'20px',
+                paddingRight:'20px',
+                height:"100vh"
+            }
+            }>
             <List
-                style={{width:'100%', backgroundColor:'#E8E8E8'}}
-                    grid={{
-                        gutter: 16,
-                        column: isMobile ? 1 :4
-                    }}
-                    bordered
-                    header={<Typography.Title level={4}>List of Projects</Typography.Title>}
-                    dataSource={projectsList}
-                    renderItem={(item) => (
-                        <List.Item
-                        >
-                            <Card 
-                                title={item.title}
-                                extra={<a href={item.link} target="_blanck">More</a>}
-                                cover={
-                                    <div style={{textAlign:'center'}}>
-                                        <Image
-                                        width='50%'
-                                        alt="example"
-                                        src={item.imagePath}
-                                    />
-                                    </div>
-                                    
-                                }
-                            >
-                                <Card.Meta 
-                                    
-                                    description={item.description}
-                                />
-                                
-                            </Card>
+                bordered
+                itemLayout="vertical"
+                style={{
+                    width:'100%', 
+                    paddingLeft:"5px",
+                    paddingRight:"5px",
+                    height:"100%"
+                }}
+                grid={{
+                    gutter: 16,
+                    column: isMobile ? 1 :4
+                }}
+                header={<Typography.Title level={4}>List of Projects</Typography.Title>}
+                dataSource={projectsList}
+                renderItem={(item) => (
+                    <List.Item
+                        style={
+                            { 
+                                borderRadius:"10px",
+                                borderWidth:"0.5px", 
+                                borderColor:"black" , 
+                                borderStyle:"solid",
+                                padding:"5px"
 
-                        </List.Item>
-                    )}
-                />
+                            }
+                        }
+
+                        actions={[<a href={item.link} target="_blank">Go to Website</a>]}
+
+                    >
+                        <div
+
+                        >
+                            <List.Item.Meta
+                            avatar={<Avatar src={item.imagePath} />}
+                            title={<a href="https://ant.design">{item.title}</a>}
+                            description={item.description}
+                            />
+                        </div>
+                            
+                    </List.Item>
+                )}
+            />
 
         </div>
 
